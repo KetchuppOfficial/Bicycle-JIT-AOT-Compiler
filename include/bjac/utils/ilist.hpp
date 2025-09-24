ac852a8a96fdc2b00cc313f8d5e2e267873c13c3
KetchuppOfficial <dolgov04@list.ru>
2025-09-24 21:50:56 +0300

    remove pointer to parent from ilist

--- include/bjac/utils/ilist.hpp
+++ include/bjac/utils/ilist.hpp
@@ -10,13 +10,11 @@
 
 namespace bjac {
 
-template <typename ValueT, typename ParentT>
+template <typename ValueT>
 class ilist_node {
   public:
-    using parent_type = std::remove_cv_t<ParentT>;
     using value_type = ValueT;
 
-    static_assert(std::is_object_v<parent_type>);
     static_assert(std::is_object_v<value_type>);
     static_assert(std::is_base_of_v<ilist_node, value_type>);
 
@@ -26,14 +24,12 @@ class ilist_node {
     ilist_node &operator=(const ilist_node &) = delete;
 
     ilist_node(ilist_node &&rhs) noexcept
-        : next_{std::exchange(rhs.next_, nullptr)}, prev_{std::exchange(rhs.prev_, nullptr)},
-          parent_{std::exchange(rhs.parent_, nullptr)} {}
+        : next_{std::exchange(rhs.next_, nullptr)}, prev_{std::exchange(rhs.prev_, nullptr)} {}
 
     ilist_node &operator=(ilist_node &&rhs) noexcept {
         if (this != std::addressof(rhs)) {
             next_ = std::exchange(rhs.next_, nullptr);
             prev_ = std::exchange(rhs.prev_, nullptr);
-            parent_ = std::exchange(rhs.parent_, nullptr);
         }
         return *this;
     }
@@ -46,10 +42,6 @@ class ilist_node {
     const ilist_node *prev() const noexcept { return prev_; }
     void set_prev(ilist_node *prev) noexcept { prev_ = prev; }
 
-    parent_type *parent() noexcept { return parent_; }
-    const parent_type *parent() const noexcept { return parent_; }
-    void set_parent(parent_type *parent) noexcept { parent_ = parent; }
-
     // Note: this methods should be called only if ilist_node is a base class subobject of ValueT
     auto &value() noexcept { return *static_cast<value_type *>(this); }
     const auto &value() const noexcept { return *static_cast<const value_type *>(this); }
@@ -57,12 +49,11 @@ class ilist_node {
   private:
     ilist_node *next_ = nullptr;
     ilist_node *prev_ = nullptr;
-    ParentT *parent_ = nullptr;
 };
 
-template <typename ValueT, typename ParentT>
+template <typename ValueT>
 class ilist_iterator final {
-    using node_type = ilist_node<ValueT, ParentT>;
+    using node_type = ilist_node<ValueT>;
 
   public:
     using difference_type = std::ptrdiff_t;
@@ -97,20 +88,20 @@ class ilist_iterator final {
     node_type *node_ = nullptr;
 };
 
-static_assert(std::bidirectional_iterator<ilist_iterator<int, int>>);
+static_assert(std::bidirectional_iterator<ilist_iterator<int>>);
 
-template <typename ValueT, typename ParentT>
-class ilist : public ilist_node<ValueT, ParentT> {
+template <typename ValueT>
+class ilist {
   public:
-    using node_type = ilist_node<ValueT, ParentT>;
+    using node_type = ilist_node<ValueT>;
     using typename node_type::parent_type;
     using typename node_type::value_type;
-    using iterator = ilist_iterator<ValueT, ParentT>;
-    using const_iterator = ilist_iterator<const ValueT, ParentT>;
+    using iterator = ilist_iterator<ValueT>;
+    using const_iterator = ilist_iterator<const ValueT>;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    ilist() noexcept { sentinel()->set_next(sentinel()); }
+    ilist() noexcept { sentinel_.set_next(&sentinel_); }
 
     iterator begin() noexcept { return {head()}; }
     const_iterator begin() const noexcept { return {head()}; }
@@ -120,8 +111,8 @@ class ilist : public ilist_node<ValueT, ParentT> {
     const_reverse_iterator rbegin() const noexcept { return {end()}; }
     const_reverse_iterator crbegin() const noexcept { return rbegin(); }
 
-    iterator end() noexcept { return {sentinel()}; }
-    const_iterator end() const noexcept { return {sentinel()}; }
+    iterator end() noexcept { return {&sentinel_}; }
+    const_iterator end() const noexcept { return {&sentinel_}; }
     const_iterator cend() const noexcept { return end(); }
 
     reverse_iterator rend() noexcept { return {begin()}; }
@@ -129,11 +120,10 @@ class ilist : public ilist_node<ValueT, ParentT> {
     const_reverse_iterator crend() const noexcept { return rend(); }
 
   private:
-    auto *head() noexcept { return sentinel()->next(); }
-    auto *head() const noexcept { return sentinel()->next(); }
+    auto *head() noexcept { return sentinel_.next(); }
+    auto *head() const noexcept { return sentinel_.next(); }
 
-    auto *sentinel() noexcept { return static_cast<node_type *>(this); }
-    auto *sentinel() const noexcept { return static_cast<const node_type *>(this); }
+    node_type sentinel_;
 };
 
 } // namespace bjac
