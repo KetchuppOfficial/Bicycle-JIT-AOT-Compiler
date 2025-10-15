b563cc1fea2d68b01563657546c6389795df9289
KetchuppOfficial <dolgov04@list.ru>
2025-10-15 17:04:25 +0300

    [cmake] use C++26

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -1,6 +1,6 @@
 add_library(defaults INTERFACE)
 add_library(bjac::defaults ALIAS defaults)
-target_compile_features(defaults INTERFACE cxx_std_23)
+target_compile_features(defaults INTERFACE cxx_std_26)
 target_compile_options(defaults
 INTERFACE
     $<$<CXX_COMPILER_ID:MSVC>:/Wall>
