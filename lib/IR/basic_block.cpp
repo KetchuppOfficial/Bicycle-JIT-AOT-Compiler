5de42cde8f732a09f2a3fb80318d7876ab07fa58
KetchuppOfficial <dolgov04@list.ru>
2025-11-18 01:01:26 +0300

    [IR] Print preds section as a comment

--- lib/IR/basic_block.cpp
+++ lib/IR/basic_block.cpp
@@ -16,7 +16,7 @@ void BasicBlock::print(std::ostream &os) const {
     os << std::format("%bb{}:", get_id());
 
     if (!predecessors_.empty()) {
-        os << " preds: ";
+        os << " ; preds: ";
         auto prev_end = std::ranges::prev(predecessors_.end());
         for (const auto *pred : std::ranges::subrange(predecessors_.begin(), prev_end)) {
             os << std::format("%bb{}, ", pred->get_id());
