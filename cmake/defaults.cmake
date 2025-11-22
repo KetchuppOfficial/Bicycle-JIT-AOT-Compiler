a231e0882765dc083a64989251ba16df9187f8d8
KetchuppOfficial <dolgov04@list.ru>
2025-11-22 14:06:15 +0300

    [cmake] Mangle names of targets with the name of the project

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -1,7 +1,7 @@
-add_library(defaults INTERFACE)
-add_library(bjac::defaults ALIAS defaults)
-target_compile_features(defaults INTERFACE cxx_std_26)
-target_compile_options(defaults
+add_library(bjac_defaults INTERFACE)
+add_library(bjac::defaults ALIAS bjac_defaults)
+target_compile_features(bjac_defaults INTERFACE cxx_std_26)
+target_compile_options(bjac_defaults
 INTERFACE
     $<$<CXX_COMPILER_ID:MSVC>:/Wall>
     $<$<CXX_COMPILER_ID:GNU,Clang>: -Wall -Wextra -pedantic>
