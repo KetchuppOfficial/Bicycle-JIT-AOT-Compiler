35e9c90f65937fabff1616e88b09f9576eeb2489
KetchuppOfficial <dolgov04@list.ru>
2026-02-21 18:51:23 +0300

    [nix] Bump version of nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "Tasks on course in JIT and AOT compilers in virtual machines";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     };
 
     outputs = {
