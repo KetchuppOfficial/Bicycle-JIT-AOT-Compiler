703b777e8054a47912b407e551db0d5b1acc8091
KetchuppOfficial <dolgov04@list.ru>
2026-02-23 09:48:58 +0300

    [IR] Fix printing functions

--- lib/IR/function.cpp
+++ lib/IR/function.cpp
@@ -1,14 +1,17 @@
 #include <ostream>
 #include <ranges>
+#include <string_view>
 
 #include "bjac/IR/function.hpp"
 
 namespace bjac {
 
 void Function::print(std::ostream &os) const {
+    using namespace std::string_view_literals;
     auto args =
         arguments_ | std::views::transform([](Type type) static { return to_string_view(type); });
-    std::println(os, "{} {}({:s})", return_type_, name_, std::views::join_with(args, ", "));
+    std::println(os, "{} {}({:s})", return_type_, name_,
+                 std::views::join_with(args, ", "sv));
     for (auto &bb : *this) {
         bb.print(os);
     }
