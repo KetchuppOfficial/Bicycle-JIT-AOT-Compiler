0c894e087c45fe5f9f713ba7733fc16bac9dfea5
KetchuppOfficial <dolgov04@list.ru>
2026-02-25 15:48:17 +0300

    [IR] Fix style

--- lib/IR/function.cpp
+++ lib/IR/function.cpp
@@ -10,8 +10,7 @@ void Function::print(std::ostream &os) const {
     using namespace std::string_view_literals;
     auto args =
         arguments_ | std::views::transform([](Type type) static { return to_string_view(type); });
-    std::println(os, "{} {}({:s})", return_type_, name_,
-                 std::views::join_with(args, ", "sv));
+    std::println(os, "{} {}({:s})", return_type_, name_, std::views::join_with(args, ", "sv));
     for (auto &bb : *this) {
         bb.print(os);
     }
